1558c6508cd655ada33eb74a6ac8f070145ac425
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-03 18:01:03 -0300

    Update calendar.py

--- components/calendar.py
+++ components/calendar.py
@@ -1,4 +1,5 @@
 import calendar
+import pandas as pd
 import streamlit as st
 
 
@@ -7,95 +8,54 @@ def gerar_calendario(ano, mes, eventos=None):
     if eventos is None:
         eventos = {}
 
-    # Semana começa no domingo
     calendar.setfirstweekday(calendar.SUNDAY)
 
-    cal = calendar.monthcalendar(ano, mes)
+    semanas = calendar.monthcalendar(ano, mes)
 
-    dias_semana = [
-        "DOM",
-        "SEG",
-        "TER",
-        "QUA",
-        "QUI",
-        "SEX",
-        "SAB"
-    ]
+    linhas = []
 
-    html = """
-    <table style="
-        width:100%;
-        border-collapse:collapse;
-        text-align:center;
-        font-family:Arial;
-    ">
-    """
+    for semana in semanas:
 
-    # Cabeçalho
-    html += "<tr>"
-
-    for dia in dias_semana:
-
-        html += f"""
-        <th style="
-            background:#E6E6E6;
-            padding:8px;
-            border:1px solid white;
-            font-size:14px;
-        ">
-            {dia}
-        </th>
-        """
-
-    html += "</tr>"
-
-    # Dias do mês
-    for semana in cal:
-
-        html += "<tr>"
+        linha = []
 
         for dia in semana:
 
             if dia == 0:
-
-                html += """
-                <td style="
-                    height:40px;
-                    border:1px solid white;
-                ">
-                </td>
-                """
-
+                linha.append("")
             else:
-
-                # Cor padrão
-                cor = "#DCE6F1"
 
                 if dia in eventos:
 
                     if eventos[dia] == "Sell In":
-                        cor = "#FCE4D6"
+                        linha.append(f"🟧 {dia}")
 
                     elif eventos[dia] == "Sell Out":
-                        cor = "#FFF2CC"
+                        linha.append(f"🟨 {dia}")
 
-                html += f"""
-                <td style="
-                    background:{cor};
-                    height:40px;
-                    border:1px solid white;
-                    font-weight:bold;
-                    font-size:15px;
-                ">
-                    {dia}
-                </td>
-                """
+                    else:
+                        linha.append(str(dia))
 
-        html += "</tr>"
+                else:
+                    linha.append(str(dia))
 
-    html += "</table>"
+        linhas.append(linha)
 
-    st.markdown(
-        html,
-        unsafe_allow_html=True
+    df = pd.DataFrame(
+        linhas,
+        columns=[
+            "DOM",
+            "SEG",
+            "TER",
+            "QUA",
+            "QUI",
+            "SEX",
+            "SAB"
+        ]
     )
+
+    st.table(df)
+
+    st.markdown("""
+    🟧 Sell In &nbsp;&nbsp;&nbsp;&nbsp;
+    🟨 Sell Out
+    """)

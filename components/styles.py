4791f2736791e4431fb1d8b982deb968082b8e5e
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-03 17:57:54 -0300

    Update styles.py

--- components/styles.py
+++ components/styles.py
@@ -6,6 +6,7 @@ def load_css():
     st.markdown("""
     <style>
 
+    /* TÍTULO PRINCIPAL */
     .main-title{
         font-size:42px;
         font-weight:700;
@@ -13,6 +14,7 @@ def load_css():
         margin-bottom:20px;
     }
 
+    /* CABEÇALHOS DE SEÇÃO */
     .section-title{
         background:#6E6E6E;
         color:white;
@@ -24,21 +26,34 @@ def load_css():
         margin-bottom:5px;
     }
 
+    /* CAIXA DE MECÂNICA */
     .mecanica-box{
-    background:white;
-    border:1px solid #CFCFCF;
-    border-radius:5px;
-    padding:15px;
-    min-height:200px;
-    color:black;
-    font-size:16px;
+        background:white;
+        border:1px solid #CFCFCF;
+        border-radius:5px;
+        padding:15px;
+        min-height:220px;
+        color:black;
+        font-size:16px;
     }
 
+    .mecanica-box ul{
+        margin:0;
+        padding-left:20px;
+    }
+
+    .mecanica-box li{
+        margin-bottom:10px;
+        line-height:1.5;
+    }
+
+    /* TÍTULO DOS CANAIS */
     .canal-title{
-        width:200px;
+        width:220px;
         margin:auto;
         text-align:center;
         background:white;
+        color:black;
         padding:8px;
         font-size:26px;
         font-weight:bold;
@@ -46,14 +61,17 @@ def load_css():
         box-shadow:0px 2px 8px rgba(0,0,0,.20);
     }
 
+    /* NOME SKU */
     .sku-name{
         text-align:center;
         font-size:14px;
         font-weight:bold;
         margin-top:5px;
         min-height:40px;
+        color:black;
     }
 
+    /* PREÇO DE */
     .old-price{
         background:#7C7C7C;
         color:white;
@@ -61,8 +79,10 @@ def load_css():
         padding:5px;
         border-radius:20px;
         margin-top:5px;
+        font-weight:bold;
     }
 
+    /* PREÇO PARA */
     .new-price{
         background:#B9B9B9;
         color:white;
@@ -70,6 +90,12 @@ def load_css():
         padding:5px;
         border-radius:20px;
         margin-top:5px;
+        font-weight:bold;
+    }
+
+    /* IMAGENS DOS PRODUTOS */
+    [data-testid="stImage"]{
+        text-align:center;
     }
 
     </style>

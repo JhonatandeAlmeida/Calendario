53233cf0c69114293628e5806fc4f45180afb9e8
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-03 17:13:01 -0300

    Add Atacado section to display product images and prices

--- components/cards.py
+++ components/cards.py
@@ -1,1 +1,28 @@
+st.markdown("## Atacado")
 
+cols = st.columns(4)
+
+atacado = produtos[
+    produtos["Canal"]=="Atacado"
+]
+
+for i, row in enumerate(atacado.iterrows()):
+
+    _, produto = row
+
+    with cols[i % 4]:
+
+        st.image(
+            f"images/produtos/{produto['Imagem']}",
+            width=90
+        )
+
+        st.markdown(
+            f"""
+            **{produto['SKU']}**
+
+            🔵 R$ {produto['Preço Atual']}
+
+            🟢 R$ {produto['Preço Promo']}
+            """
+        )

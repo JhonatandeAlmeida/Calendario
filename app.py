715b287375d88e5e7b1732c02faff1671bb93471
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-04 09:36:02 -0300

    Refactor loop and indentation in app.py

--- app.py
+++ app.py
@@ -338,50 +338,46 @@ def mostrar_produtos(df_canal, canal):
 
         cols = st.columns(6)
 
-        for i, (_, row) in enumerate(
-            produtos_q.iterrows()
-        ):
+for i, (_, row) in enumerate(produtos_q.iterrows()):
 
-        with cols[i % 6]:
+    with cols[i % 6]:
 
-            try:
-                c1, c2, c3 = st.columns([1, 2, 1])
+        try:
 
-            with c2:
-                st.image(
-                    f"images/produtos/{row['Imagem']}",
-                    width=85
-                )
+            st.image(
+                f"images/produtos/{row['Imagem']}",
+                width=85
+            )
 
-                except Exception:
-                    st.empty()
+        except Exception:
+            st.empty()
 
-                st.markdown(
-                    f"""
-                    <div class='sku-name'>
-                        {row['SKU']}
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )
+        st.markdown(
+            f"""
+            <div class="sku-name">
+                {row['SKU']}
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
-                st.markdown(
-                    f"""
-                    <div class='old-price'>
-                        R$ {float(row['De']):.2f}
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )
+        st.markdown(
+            f"""
+            <div class="old-price">
+                R$ {float(row['De']):.2f}
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
-                st.markdown(
-                    f"""
-                    <div class='new-price'>
-                        R$ {float(row['Para']):.2f}
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )
+        st.markdown(
+            f"""
+            <div class="new-price">
+                R$ {float(row['Para']):.2f}
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
 # ==================================================
 # EXIBIÇÃO DOS CANAIS

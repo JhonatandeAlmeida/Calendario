1a72c458961274a681143fe05685e13ffd9dd671
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-09 17:59:39 -0300

    Fix image display height in app.py

--- app.py
+++ app.py
@@ -310,6 +310,7 @@ def mostrar_produtos(df_canal, canal, negocio):
                         st.image(
                             f"images/produtos_padronizados/{row['Imagem']}",
                             width=largura_imagem
+                            height=170
                         )
 
                 except Exception:

96725cf16832459002a3f7064c58106919e53524
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-09 15:20:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -546,6 +546,51 @@ with col_esquerda:
         """,
         unsafe_allow_html=True
     )
+    sell_in = mecanica_mes[
+        mecanica_mes["Tipo"]
+        .astype(str)
+        .str.upper()
+        == "SELL IN"
+    ]
+    
+    sell_out = mecanica_mes[
+        mecanica_mes["Tipo"]
+        .astype(str)
+        .str.upper()
+        == "SELL OUT"
+    ]
+    
+    html = "<div class='mecanica-box'>"
+    
+    if not sell_in.empty:
+    
+        html += """
+        <div class='mecanica-subtitle'>
+            SELL IN
+        </div>
+        <ul class='mecanica-lista'>
+        """
+    
+        for _, row in sell_in.iterrows():
+            html += f"<li>{row['Texto']}</li>"
+    
+        html += "</ul>"
+    
+    if not sell_out.empty:
+    
+        html += """
+        <div class='mecanica-subtitle'>
+            SELL OUT
+        </div>
+        <ul class='mecanica-lista'>
+        """
+    
+        for _, row in sell_out.iterrows():
+            html += f"<li>{row['Texto']}</li>"
+    
+        html += "</ul>"
+    
+    html += "</div>"
 
     st.markdown(
         html,

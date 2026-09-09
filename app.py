e0b728ba03f507de418a1f7fcd742674410565f3
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-09 17:02:56 -0300

    Adjust scaling factor in image resizing

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def padronizar_imagem(caminho_imagem):
     proporcao = min(
         tamanho / img.width,
         tamanho / img.height
-    ) * 2
+    ) * 1
 
     nova_largura = int(img.width * proporcao)
     nova_altura = int(img.height * proporcao)

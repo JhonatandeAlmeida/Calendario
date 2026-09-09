534296c2eb40b226272755b406178a31bff6746b
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-09 17:16:39 -0300

    Adjust image size parameters in padronizar_imagem

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def padronizar_imagem(caminho_imagem):
 
     img = Image.open(caminho_imagem).convert("RGBA")
 
-    tamanho = 1000
+    tamanho = 600
 
     canvas = Image.new(
         "RGBA",
@@ -35,7 +35,7 @@ def padronizar_imagem(caminho_imagem):
     proporcao = min(
         tamanho / img.width,
         tamanho / img.height
-    ) * 1.5
+    ) * 1.25
 
     nova_largura = int(img.width * proporcao)
     nova_altura = int(img.height * proporcao)

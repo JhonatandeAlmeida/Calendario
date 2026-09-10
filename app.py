77102e386278e26b09107e747b6655468b271df1
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-10 09:53:49 -0300

    Adjust column widths in Streamlit layout

--- app.py
+++ app.py
@@ -307,7 +307,7 @@ def mostrar_produtos(df_canal, canal, negocio):
 
                 try:
 
-                    c1, c2, c3 = st.columns([1, 3, 1])
+                    c1, c2, c3 = st.columns([0.5, 3, 0.5])
 
                     with c2:
                         st.image(

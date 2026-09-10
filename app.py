a97dd2e0d720293d919772e0ded317bbbe52ea9f
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-10 09:23:31 -0300

    Fix image display in app.py with container width

--- app.py
+++ app.py
@@ -310,6 +310,7 @@ def mostrar_produtos(df_canal, canal, negocio):
 
                     st.image(
                         f"images/produtos_padronizados/{row['Imagem']}",
+                        use_container_width=True,
                         width=220
                     )
 

fad930f87c090e31d2267fb722c2b0a319ce692a
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-10 08:54:26 -0300

    Change qtd_colunas value for 'CERV' condition

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ def mostrar_produtos(df_canal, canal, negocio):
         )
 
         if negocio.upper() == "CERV":
-            qtd_colunas = 6
+            qtd_colunas = 9
             largura_imagem = 180
         else:
             qtd_colunas = 5

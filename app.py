2ebc155bfc982a103691c5f0df16a0d7c14da758
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-10 09:48:27 -0300

    Set image width to 100 in app.py

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ def mostrar_produtos(df_canal, canal, negocio):
                     with c2:
                         st.image(
                             f"images/produtos_padronizados/{row['Imagem']}",
-                            use_container_width=True
+                            width=100
                         )
 
                 except Exception:

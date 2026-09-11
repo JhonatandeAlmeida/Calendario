8e097b42adc188bd9b9759cbc3c8a76b00921066
JhonatandeAlmeida <99823517@ambev.com.br>
2026-09-11 09:30:40 -0300

    Adjust column widths in Streamlit layout

--- app.py
+++ app.py
@@ -505,7 +505,7 @@ meses = sorted(
     .unique()
 )
 
-colf1, colf2, colf3, colf4, colf5, colf6, colf7, colf8 = st.columns([1,2,1,2,1,2,1,2])
+colf1, colf2, colf3, colf4, colf5, colf6, colf7, colf8 = st.columns([0.5,2,0.5,2,0.5,2,0.5,2])
 
 with colf1:
     st.markdown(
